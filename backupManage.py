d8ac7e0d6c38353f4d2155f3c3bff6d224a31beb
Bob Kwiencien <bkwiencien@gmail.com>
2020-11-14 17:14:16 -0500

    initial commit

---
+++ backupManage.py
@@ -0,0 +1,33 @@
+#!/Users/bobkwiencien/anaconda/bin/python
+import sys
+import datetime as dt
+import datedelta
+import pdb
+import os
+import glob
+import pdb 
+
+def walPurge(argo):
+  ddate = argo[1]
+  try:
+        checkdate = dt.datetime.strptime(sys.argv[1], '%Y-%m-%d')
+  except ValueError:
+        raise ValueError("Incorrect date format, should be YYYY-MM-DD")      
+  pgdata = os.environ["PGDATA"]
+  waldir = pgdata+"/pg_wal"
+  statusdir = waldir+"/archive_status"
+  print(waldir)
+  listOfFiles=os.listdir(waldir)
+  listo=[]
+  datelist=[]
+  for f in listOfFiles:
+  	if os.path.isfile(os.path.join(waldir, f)):
+  		listo.append(waldir+"/"+f)
+  print(listo)
+  return
+if __name__=='__main__':
+  if (len(sys.argv) < 2):
+    exit("no date passed")	
+  walPurge(sys.argv)  
+  print("Done success as expected")
+
---
+++ doBackup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+export PATH=.:$PATH
+PGDATA=/usr/local/var/postgres
+BACKUP=/usr/local/Cellar/postgresql/12.1/bin/pg_basebackup
+DIRBASE=/Users/bobkwiencien/backups
+MKDIR=/bin/mkdir
+findDir()  {
+a=`date "+%Y-%m-%d"`
+diro=$DIRBASE/$a
+if [ -f $diro ]; then
+:
+else
+  $MKDIR $diro
+fi
+}
+findDir
+$BACKUP -D $diro -Ft -z -X stream
+backupManage.py $a
