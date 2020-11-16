2d664ac034135b25eaea448e6654e12bac24bbc1
Bob Kwiencien <bkwiencien@gmail.com>
2020-11-16 10:32:55 -0500

    figured out linux time conversion

--- walManage.py
+++ walManage.py
@@ -5,24 +5,32 @@ import sys
 import datetime as dt
 
 def walPurge(argo):
-  ddate = argo[1]
-  try:
-        checkdate = dt.datetime.strptime(sys.argv[1], '%Y-%m-%d')
-  except ValueError:
-        raise ValueError("Incorrect date format, should be YYYY-MM-DD")      
-  pgdata = os.environ["PGDATA"]
-  waldir = pgdata+"/pg_wal"
-  statusdir = waldir+"/archive_status"
-  listOfFiles=os.listdir(waldir)
-  listo=[]
-  datelist=[]
-  for f in listOfFiles:
-  	if (os.path.isfile(os.path.join(waldir, f))):
-  		listo.append(waldir+"/"+f)
-  print(listo)    
-  return
+	ddate = argo[1]
+	#pdb.set_trace()
+	try:
+				checkdate = dt.datetime.strptime(sys.argv[1], '%Y-%m-%d')
+				print("Checkdate = ".format(checkdate))
+	except ValueError:
+				raise ValueError("Incorrect date format, should be YYYY-MM-DD")      
+	pgdata = os.environ["PGDATA"]
+	waldir = pgdata+"/pg_wal"
+	archivedir = pgdata+"/archive"
+	statusdir = waldir+"/archive_status"
+	listOfFiles=os.listdir(waldir)
+	listo=[]
+	datelist=[]
+	for f in listOfFiles:
+		if (os.path.isfile(os.path.join(waldir, f))):
+			listo.append(waldir+"/"+f)
+			ctime=os.path.getctime(waldir+"/"+f)
+			filetime=dt.datetime.fromtimestamp(ctime)
+			print("checkdate {}".format(checkdate))
+			print("filetime {}".format(filetime))
+			if checkdate > filetime:
+				print("i will delete the bitch")
+	return
 if __name__=='__main__':
-  if (len(sys.argv) < 2):
-    exit("no date passed")	
-  walPurge(sys.argv)  
-  print("Done success as expected")
+	if (len(sys.argv) < 2):
+		exit("no date passed")	
+	walPurge(sys.argv)  
+	print("Done success as expected")
