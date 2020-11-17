ba8a61cd46c379c7ddd61f9fb52f10cd76c321b9
Bob Kwiencien <bkwiencien@gmail.com>
2020-11-17 14:51:23 -0500

    more and more refactoring

--- fileManager.py
+++ fileManager.py
@@ -16,27 +16,13 @@ def walPurge(argo):
 	ddate = argo[1]
 	try:
 				checkdate = dt.datetime.strptime(sys.argv[1], '%Y-%m-%d')
-				print("Checkdate = ".format(checkdate))
 	except ValueError:
 				raise ValueError("Incorrect date format, should be YYYY-MM-DD")      
 	pgdata = os.environ["PGDATA"]
 	waldir = pgdata+"/pg_wal"
-	archivedir = pgdata+"/archive"
-	statusdir = waldir+"/archive_status"
-	listOfWalFiles=os.listdir(waldir)
-	for f in listOfWalFiles:
-		if (os.path.isfile(os.path.join(waldir, f))):
-			ctime=os.path.getctime(waldir+"/"+f)
-			filetime=dt.datetime.fromtimestamp(ctime)
-			if checkdate > filetime:
-				os.remove(waldir+"/"+f)
-	listOfArchiveFiles=os.listdir(archivedir)			
-	for ff in listOfArchiveFiles:
-		if (os.path.isfile(os.path.join(archivedir, ff))):
-			ctime=os.path.getctime(archivedir+"/"+ff)
-			filetime=dt.datetime.fromtimestamp(ctime)
-			if checkdate > filetime:
-				os.remove(archivedir+"/"+ff)			
+	removeOtioseFiles(checkdate,pgdata+"/pg_wal")
+	removeOtioseFiles(checkdate,pgdata+"/archive")
+	removeOtioseFiles(checkdate,pgdata+"/archive_status")		
 	return
 if __name__=='__main__':
 	if (len(sys.argv) < 2):
